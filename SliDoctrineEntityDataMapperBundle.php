04a3e6ee7482cf80429e6757fc98de658fdf4d2e
Jacob Boswell <jacob@s56.net>
2015-05-18 10:51:58 -0600

    Added line to register extension

--- SliDoctrineEntityDataMapperBundle.php
+++ SliDoctrineEntityDataMapperBundle.php
@@ -2,6 +2,7 @@
 
 namespace Sli\DoctrineEntityDataMapperBundle;
 
+use Sli\DoctrineEntityDataMapperBundle\DependencyInjection\SliDoctrineEntityDataMapperExtension;
 use Sli\ExpanderBundle\Ext\ExtensionPoint;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
@@ -14,5 +15,10 @@ class SliDoctrineEntityDataMapperBundle extends Bundle
         $valueConverterProviders = new ExtensionPoint('sli_doctrine_entity_data_mapper.complex_field_value_converters');
         $valueConverterProviders->setDescription('Allows to contribute custom value converters');
         $container->addCompilerPass($valueConverterProviders->createCompilerPass());
+    }
+
+    public function getContainerExtension()
+    {
+        return new SliDoctrineEntityDataMapperExtension();
     }
 }
